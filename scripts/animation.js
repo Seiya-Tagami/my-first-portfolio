fde0fc1e9c3c96cebf4770e1c8316bb91136d7d7
Seiya-Tagami <bepozurapu274@gmail.com>
2023-02-16 19:21:38 +0900

    feat: scroll up button and footer

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,3 +1,22 @@
 "use strict";
 
+{
+  // appear scroll up
+  const scrollUpButton = document.querySelector(".js-scroll-up");
+  const screenHeight = window.innerHeight;
+  const appearPoint = screenHeight * 0.7;
+  const triggerLocation = document.getElementById("works");
 
+  window.addEventListener("scroll", () => {
+    const elementHeightTotop = triggerLocation.getBoundingClientRect().top;
+    if (elementHeightTotop < appearPoint) {
+      scrollUpButton.classList.add("u-block");
+    } else {
+      scrollUpButton.classList.remove("u-block");
+    }
+  });
+
+  scrollUpButton.addEventListener("click", () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+  });
+}
